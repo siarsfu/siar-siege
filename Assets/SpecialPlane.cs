738fef5095ba9d1276333307292885a2000e2724
redfoxgamedev <devryzhov@gmail.com>
2019-03-28 16:53:23 -0700

    Add paper plane throw

---
+++ Assets/SpecialPlane.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialPlane : MonoBehaviour {
+
+    private Rigidbody physics;
+    private OVRGrabbable grabbable;
+    private PlaneControl plane;
+    public Transform rightHandAnchor;
+
+    private bool wasGrabbed;
+
+	// Use this for initialization
+	void Start () {
+        physics = this.GetComponent<Rigidbody>();
+        grabbable = this.GetComponent<OVRGrabbable>();
+        plane = this.GetComponent<PlaneControl>();
+
+        wasGrabbed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (wasGrabbed && !grabbable.isGrabbed)
+        {
+            wasGrabbed = false;
+            //physics.velocity = Vector3.zero;
+            physics.angularVelocity = Vector3.zero;
+
+            //Vector3 direction 
+            physics.AddForce(rightHandAnchor.forward*25f, ForceMode.Impulse);
+            plane.enabled = true;
+        }
+        else if (grabbable.isGrabbed)
+        {
+            wasGrabbed = true;
+        }
+
+	}
+}
