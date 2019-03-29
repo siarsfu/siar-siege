6eb975d2af458e504d339ff387464c05d1e117b2
redfoxgamedev <devryzhov@gmail.com>
2019-03-29 16:12:52 -0700

    Add menu room, fade to black

---
+++ Assets/FadingController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadingController : MonoBehaviour {
+
+    private Animator fadingAnimator;
+
+    public AnimationClip fadeToBlack;
+    public AnimationClip fadeToWhite;
+
+	// Use this for initialization
+	void Start () {
+        fadingAnimator = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void fadeToBlackIn(float secondsBeforeFadingBlack)
+    {
+        StartCoroutine(fadeToBlackAfterSeconds(secondsBeforeFadingBlack));
+    }
+
+    IEnumerator fadeToBlackAfterSeconds(float seconds){
+        yield return new WaitForSeconds(seconds);
+        fadingAnimator.Play(fadeToBlack.name);
+    }
+}
---
+++ Assets/FlowControl.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+using TMPro;
+
+public class FlowControl : MonoBehaviour {
+
+    public VideoPlayer menuVideo;
+    public float secondsBeforeExperienceBegins = 2f;
+    public TextMeshProUGUI menuText;
+    public float secondsBeforeFadingBlack = 2f;
+    public FadingController fadingController;
+
+	// Use this for initialization
+	void Start () {
+        //the experience starts with small pause after which the menu appears
+        StartCoroutine(beginningPause(secondsBeforeExperienceBegins));
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator beginningPause(float seconds){
+        yield return new WaitForSeconds(seconds);
+        showVideo();
+    }
+
+    private void showVideo(){
+
+        VideoClip menuClip = menuVideo.clip;
+        double menuClipLength = menuClip.length;
+        Debug.Log("Video is " + menuClipLength + " secoonds long");
+
+        //so after the animation was finished, display the text
+        StartCoroutine(showMenuTextAfterSeconds((float)menuClipLength));
+
+        menuVideo.Play();
+    }
+
+    IEnumerator showMenuTextAfterSeconds(float seconds){
+        Debug.Log("Showing text after " + seconds+" seconds");
+        yield return new WaitForSeconds(seconds);
+        menuText.enabled = true;
+
+        //fade to black after some time passes
+        fadingController.fadeToBlackIn(secondsBeforeFadingBlack);
+        //StartCoroutine(fadeToBlackAfterSeconds(secondsBeforeFadingBlack));
+
+    }
+
+    private void transitionToThroneRoom(){
+
+    }
+
+    //first - fade to black
+    //second - teleport
+    //both need to be precisely timed
+    //IEnumerator fadeToBlackAfterSeconds(float seconds){
+    //    yield return new WaitForSeconds(seconds);
+    //    fadeToBlack();
+    //}
+
+    //private void fadeToBlack(){
+    //    fadingAnimator.enabled = true;
+      
+    //}
+
+     
+}
