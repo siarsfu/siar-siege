191c4a3ba7ecb7ff9be827e6d9a2f1ff4250e06d
redfoxgamedev <devryzhov@gmail.com>
2019-03-30 14:35:14 -0700

    Add fan pick up glow

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -18,6 +18,6 @@ public class GameManager : MonoBehaviour {
 
     public void begin()
     {
-        planeGenerator.enabled = true;
+        //planeGenerator.enabled = true;
     }
 }
---
+++ Assets/PickUpControl.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpControl : MonoBehaviour {
+
+    //private static Vector3 VECTOR_CEILING = Vector3.up * 3f;
+
+    public float rotatingSpeed = 50f;
+    public float hoveringSpeed = 50f;
+
+    public bool isWaiting;
+
+    private bool isGoingUp;
+    private Vector3 vectorCeiling;
+    private Vector3 vectorFloor;
+
+	// Use this for initialization
+	void Start () {
+        isWaiting = true;
+
+        isGoingUp = true;
+        vectorCeiling = this.transform.position + this.transform.up*0.125f;
+        vectorFloor = this.transform.position - this.transform.up*0.125f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isWaiting)
+        {
+            this.transform.Rotate(Vector3.up, rotatingSpeed * Time.deltaTime);
+
+            hoverMotion();
+            //this.transform.Translate(Vector3.up * hoveringSpeed * Time.deltaTime);
+        }
+
+
+	}
+
+    private void hoverMotion()
+    {
+        if (isGoingUp)
+        {
+            this.transform.position = Vector3.Lerp(this.transform.position, vectorCeiling, hoveringSpeed * Time.deltaTime);
+        }
+        else
+        {
+            this.transform.position = Vector3.Lerp(this.transform.position, vectorFloor, hoveringSpeed * Time.deltaTime);
+        }
+
+        if (closeToCeiling())
+        {
+            isGoingUp = false;
+        }
+        else if (closeToFloor())
+        {
+            isGoingUp = true;
+        }
+
+    }
+
+    private bool closeToFloor()
+    {
+        Vector3 currentPos = this.transform.position;
+
+        float distance = Vector3.Distance(currentPos, vectorFloor);
+
+        if (distance < 0.1f)
+            return true;
+        return false;
+    }
+
+    private bool closeToCeiling()
+    {
+        Vector3 currentPos = this.transform.position;
+
+        float distance = Vector3.Distance(currentPos, vectorCeiling);
+
+        if (distance < 0.1f)
+            return true;
+        return false;
+    }
+}
