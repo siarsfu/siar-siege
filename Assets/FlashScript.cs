0550300dcc4639f684f26af8b3abb93ed5616295
redfoxgamedev <devryzhov@gmail.com>
2019-04-01 11:09:48 -0700

    Add plane throw animation

---
+++ Assets/FlashScript.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashScript : MonoBehaviour
+{
+
+    public Rigidbody physics;
+    public float currentVelocity;
+    public CharacterController character;
+
+    public GameObject bullet;
+
+    public FlyingAnimationRender animationRender;
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+
+        if (userCanShoot())
+        {
+            animationRender.Play();
+            if (Input.GetMouseButtonDown(0))
+            {
+                shootStuff();
+            }
+
+
+        } else {
+            animationRender.Stop();
+        }
+
+
+    }
+
+    private void showTrajectory()
+    {
+        animationRender.Play();
+    }
+
+    private bool userCanShoot()
+    {
+        RaycastHit hit;
+        int layerMask = 1 << 2;
+        layerMask = ~layerMask;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 150f, layerMask))
+        {
+            //Debug.Log(hit.transform.name);
+            if (hit.transform.name == "plane_hit")
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void shootStuff()
+    {
+        Debug.Log("Shoot");
+
+        GameObject anotherBullet = Instantiate(bullet, transform.position, transform.rotation);
+        anotherBullet.GetComponent<Rigidbody>().AddForce(transform.forward * 50f, ForceMode.Impulse);
+    }
+
+    private void slowTime()
+    {
+        Time.timeScale = 0.5f;
+    }
+}
---
+++ Assets/FlyingAnimationRender.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingAnimationRender : MonoBehaviour {
+
+    private LineRenderer lineRenderer;
+    public GameObject objectToRender;
+
+    public float timePeriod = 1f;
+
+    private Vector3[] allLinePositions;
+
+    private int totalPositionSize;
+    private int currentIndex;
+
+
+    private IEnumerator loop;
+    public bool animationRunning = false;
+
+	// Use this for initialization
+	void Start () {
+        lineRenderer = this.GetComponent<LineRenderer>();
+        currentIndex = 0;
+
+        totalPositionSize = lineRenderer.positionCount;
+        allLinePositions = new Vector3[totalPositionSize];
+        lineRenderer.GetPositions(allLinePositions);
+
+        //StartCoroutine(animationLoop());
+        loop = animationLoop();
+        //animationRunning = false;
+
+        StartCoroutine(loop);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    IEnumerator animationLoop(){
+
+       // animationRunning = true;
+        Debug.Log("Setting animation to run!");
+        while(true){
+
+            while (animationRunning)
+            {
+                //Debug.Log("Current index is " + currentIndex);
+                int size = lineRenderer.GetPositions(allLinePositions);
+
+                //Debug.Log("Position of curren vertex is " + allLinePositions[currentIndex]);
+
+                objectToRender.transform.localPosition = allLinePositions[currentIndex];
+
+                //need to determine rotation
+                //difference between current position and next
+                Vector3 currentPosition = allLinePositions[currentIndex];
+                int nextIndex = (currentIndex + 1) % totalPositionSize;
+
+
+                if (nextIndex != 0)
+                {
+                    //take next and approximate
+                    Vector3 nextPosition = allLinePositions[nextIndex];
+
+                    Vector3 direction = (nextPosition - currentPosition).normalized;
+
+                    //Vector3 localForward = objectToRender.transform.worldToLocalMatrix.MultiplyVector(objectToRender.transform.forward);
+
+                    Quaternion newRotation = Quaternion.FromToRotation(Vector3.forward, direction);
+
+                    //objectToRender.transform.forward = objectToRender.transform.localToWorldMatrix.MultiplyVector(direction);
+
+                    objectToRender.transform.localRotation = newRotation;
+
+                }
+                else
+                {
+                    //take previous and approximate
+                }
+
+                //iterate next
+                currentIndex = (currentIndex + 1) % totalPositionSize;
+                yield return new WaitForSeconds(timePeriod);
+            }
+            yield return null;
+        }
+
+        yield return null;
+
+    }
+
+    public void Stop()
+    {
+    
+        animationRunning = false;
+        objectToRender.transform.position = Vector3.zero;
+        currentIndex = 0;
+    }
+
+    public void Play()
+    {
+
+        animationRunning = true;
+    }
+}
