d88a5f75ad9a712129ac1a9d1a2fa897c71de3b9
redfoxgamedev <devryzhov@gmail.com>
2019-04-14 18:19:33 -0700

    Add free look camera controls

---
+++ Assets/Scripts/FlyCamera.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class FlyCamera : MonoBehaviour {
+
+    public GameObject flyingCamera;
+    public GameObject playerObject;
+
+    public bool isFlyingCamActive = true;
+
+    private FirstPersonController playerController;
+    private Camera playerCamera;
+
+	// Use this for initialization
+	void Start () {
+        playerController = playerObject.GetComponent<FirstPersonController>();
+        playerCamera = playerObject.GetComponentInChildren<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.M)){
+            switchToFlyingCamera();
+        }
+	}
+
+    private void switchToFlyingCamera()
+    {
+        isFlyingCamActive = !isFlyingCamActive;
+
+        if (isFlyingCamActive){
+            flyingCamera.SetActive(true);
+            playerController.enabled = false;
+            playerCamera.enabled = false;
+
+        } else{
+            flyingCamera.SetActive(false);
+            playerController.enabled = true;
+            playerCamera.enabled = true;
+        }
+
+    }
+}
---
+++ Assets/Scripts/PilotCamera.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PilotCamera : MonoBehaviour {
+
+    public float moveSpeed = 5f;
+    public Transform childCameraTransform;
+
+    private float initialSpeed;
+    private Vector2 rotation = new Vector2(0, 0);
+	// Use this for initialization
+	void Start () {
+        Cursor.visible = false;
+        initialSpeed = moveSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Movement();
+  
+
+
+
+
+    }
+
+ 
+
+    public void Movement(){
+
+        if (Input.GetKeyDown(KeyCode.LeftShift)){
+            moveSpeed *= 2;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift)){
+            moveSpeed = initialSpeed;
+        }
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            this.transform.Translate(childCameraTransform.transform.forward * moveSpeed * Time.deltaTime, Space.World);
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            this.transform.Translate(-childCameraTransform.transform.forward * moveSpeed * Time.deltaTime, Space.World);
+        }
+
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            this.transform.Translate(childCameraTransform.transform.right * moveSpeed * Time.deltaTime, Space.World);
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            this.transform.Translate(-childCameraTransform.transform.right * moveSpeed * Time.deltaTime, Space.World);
+        }
+    }
+}
